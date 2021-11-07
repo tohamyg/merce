e1e83be1b3e5dd6f5a80d6fea72c273e6cf6a237
MahmoudTalaat96 <mahmoud.talaat4200@gmail.com>
2021-11-07 22:04:28 +0200

    changes in html
    some changes in html

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
  
-
-
-
- // Set the date we're counting down to
- var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
- 
- // Update the count down every 1 second
- var x = setInterval(function() {
- 
-   // Get today's date and time
-   var now = new Date().getTime();
- 
-   // Find the distance between now and the count down date
-   var distance = countDownDate - now;
- 
-   // Time calculations for days, hours, minutes and seconds
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
- 
-   // Display the result in the element with id="demo"
-   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-   + minutes + "m " + seconds + "s ";
- 
-   // If the count down is finished, write some text
-   if (distance < 0) {
-	 clearInterval(x);
-	 document.getElementById("demo").innerHTML = "EXPIRED";
-   }
- }, 1000);
+ var span=document.getElementById("span");
+ var menu=document.querySelector(".menu");
+ span.onclick=function(){
+	menu.classList.toggle("show");
+    span.classList.toggle("hide");
+ }
 
   var items = document.querySelectorAll(".timeline li");
 
@@ -59,12 +34,6 @@
   window.addEventListener("scroll", callbackFunc);
   
  
- var span=document.getElementById("span");
- var menu=document.querySelector(".menu");
- span.onclick=function(){
-	menu.classList.toggle("show");
-    span.classList.toggle("hide");
- }
 
 // Init Stats
 var stats = new Stats();
